cfbaa22cbfba475297d01a1c988c2d18406d4002
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-11-08 04:05:14 +0000

    Adding a bit of test for variables

---
+++ test.py
@@ -0,0 +1,10 @@
+# variables
+integer = 6  # integer
+num_float = 6.53  # float
+string1 = "senyum"  # string
+string2 = 'gelak'
+boolean = True  # Boolean(True/false)
+not_boolean = False
+num_list = [1, "dua", 3, 4, 5]  # List of string, float, integer
+num_set = {1, "dua", 3, 3, 4, 5}  # Set of string, float, integer
+print(num_set)
