861e2ce422c9f03261293c71f08cfed4935499b4
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-11-16 22:56:17 +0000

    Small update

---
+++ operations.py
@@ -0,0 +1,22 @@
+# Arithmetic Operation
+x = 5
+y = 2
+
+add = x+y
+sub = x-y
+multi = x*y
+divi = x/y
+floor_divi = x//y
+mod = x % y
+exp = x**y
+
+# Comparison Operation
+num1 = 5
+num2 = 7
+a = "hello"
+b = "world"
+
+equality = x == y  # Result: False
+inequality = a != b  # Result: True
+g_than = num1 > num2
+g_than_equal = num1 >= num2
