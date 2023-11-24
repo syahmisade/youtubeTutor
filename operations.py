351dca26bae86802013dfb0978299a99704a9910
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-11-24 11:08:32 +0000

    Updating tuple operation

--- operations.py
+++ operations.py
@@ -185,8 +185,6 @@ items = bio_aku.items()
 # *Result: dict_items([('nama', 'Syahmi'), ('umur', 22), ('bandar', 'Bangi')])
 copy_dict = bio_aku.copy()
 # bio_aku.clear()
-
-# --- Checking if a key exist ---
 key_exists = 'age' in bio_aku  # *Result: True
 
 # --- Creating dictionaries dynamically ---
@@ -196,22 +194,24 @@ nombor = {x: x**2 for x in range(5)}
 umur = bio_aku.get('umur', 'N/A')
 # Returns 'N/A' kalau 'umur' key is not present
 
-# ----- Tuple -----
-my_tuple = (1, 2, 3, 'apple', 'banana')
-tuple1 = (1, 2, 3)
-tuple2 = ('apple', 'banana')
 
-indexing = my_tuple[0]  # *Result: 1
-slicing = my_tuple[1:4]  # *Result: (2, 3, 'apple')
-concatenated_tuples = tuple1 + tuple2  # *Result: (1, 2, 3, 'apple', 'banana')
-repeated_tuple = my_tuple * 2
-# *Result: (1, 2, 3, 'apple', 'banana', 1, 2, 3, 'apple', 'banana')
-count_tuple = my_tuple.count('apple')  # *Result: 1
-index_tuple = my_tuple.index('banana')  # *Result: 4
-tuple_length = len(my_tuple)  # *Result: 5
-element_present = 'apple' in my_tuple  # *Result: True
-# my_tuple[0] = 10  # return error sbb tuple immutable
+# ----- Tuple -----
+cth_tuple = (2, 3, 'kucing', 'berlari', 'kucing')
+tuple1 = (1, 2, 3)
+tuple2 = ('kiri', 'kanan')
+
+indexing = cth_tuple[0]  # *Result: 2
+slicing = cth_tuple[1:4]  # *Result: (3, 'kucing', 'berlari')
+concatenated_tuples = tuple1 + tuple2  # *Result: (1, 2, 3, 'kiri', 'kanan')
+repeated_tuple = cth_tuple * 2
+# *Result: (2, 3, 'kucing', 'berlari', 'kucing', 2, 3, 'kucing', 'berlari', 'kucing')
+count_tuple = cth_tuple.count('kucing')  # *Result: 2
+index_tuple = cth_tuple.index('berlari')  # *Result: 3
+tuple_length = len(cth_tuple)  # *Result: 5
+element_present = 'kucing' in cth_tuple  # *Result: True
+
+# cth_tuple[0] = 10  # return error sbb tuple immutable
 
 # --- Packing and Unpacking ---
-packed_tuple = 1, 'apple', 3.14  # *Result: (1, 'apple', 3.14)
+packed_tuple = 'Saya', 'Suka', 'Nasi Ayam'  # *Result: (1, 'apple', 3.14)
 x, y, z = packed_tuple  # *Result: x = 1, y = 'apple', z = 3.14
