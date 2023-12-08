b63ac982863b2ea084ac9ece1b78abb08c0b4d66
Syahmi <amuhamadsade@sheffield.ac.uk>
2023-12-08 19:22:05 +0000

    Create conditional statement folder

---
+++ ConditionalStatements/ifstatement.py
@@ -0,0 +1,21 @@
+# kehenseman = 40
+
+# if kehenseman >= 70:
+#     print("Jemput masuk ke kelab orang2 hensem")
+# elif kehenseman >= 50:
+#     print("Hmm boleh la masuk kelab ni")
+# else:
+#     print("Takleh masuk sebab awok tak hensem")
+
+x = 2
+y = 5
+
+if x > 5:
+    print("x is greater than 5")
+
+    if y > 2:
+        print("y is also greater than 2")
+    else:
+        print("y is not greater than 2")
+else:
+    print("x is not greater than 5")
